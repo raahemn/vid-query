94d8663e63142ee33779d1e6ccce1b870a7731e1
raahemn <25100091@lums.edu.pk>
2025-07-18 02:10:31 +0500

    Local persistance added to the vectors

--- backend/app/services/analyze_service.py
+++ backend/app/services/analyze_service.py
@@ -20,9 +20,20 @@ splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 # Will store this globally or in a cache/db later
 video_vectorstores: dict[str, FAISS] = {}
 
+VECTORSTORE_DIR = "vectorstores"
+
 async def analyze_youtube_video(video_id: str) -> str:
     if video_id in video_vectorstores:
         return f"Your video {video_id} has already been processed."
+
+    vector_path = os.path.join(VECTORSTORE_DIR, video_id)
+    
+    if os.path.exists(vector_path):
+        print(f"Loading existing vectorstore for video {video_id} from {vector_path}.")
+        vectorstore = FAISS.load_local(vector_path, embedder, allow_dangerous_deserialization=True)
+        video_vectorstores[video_id] = vectorstore
+        return f"Your video {video_id} has been loaded from the existing vectorstore."
+    
     
     transcript = fetch_youtube_transcript(video_id)
     # transcript = (
@@ -55,5 +66,9 @@ async def analyze_youtube_video(video_id: str) -> str:
 
     # Store in global dict (or replace with Redis / local file later)
     video_vectorstores[video_id] = vectorstore
+    
+    # Save to persistent storage
+    os.makedirs(VECTORSTORE_DIR, exist_ok=True)
+    vectorstore.save_local(vector_path)
 
     return f"Your video {video_id} has been processed and the vector store has been initialized!"
